1c19e670f927392ca6a4a7c13431551876672985
Yuri Ressiguier <yuri_ressiguier@id.uff.br>
2021-06-29 18:52:47 -0300

    Linha "teste" adicionado

--- src/dia0/Main.java
+++ src/dia0/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Olá Mundo!");
+        System.out.println("Teste");
         System.out.println(System.getProperty("java.version"));
     }
 }
