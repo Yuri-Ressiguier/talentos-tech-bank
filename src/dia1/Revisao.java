6c28b91e0ba31a53aaf211583fb047f8e7f45333
Yuri Ressiguier <yuri_ressiguier@id.uff.br>
2021-06-30 19:36:24 -0300

    Revisão aula 2

--- src/dia1/Revisao.java
+++ src/dia1/Revisao.java
@@ -1,2 +1,27 @@
-package dia1;public class Revisao {
+package dia1;
+
+public class Revisao {
+
+    public static void main(String[] args) {
+        short s = 123;
+        int i = s;
+        long l = i;
+        float f = l;
+        double d = f;
+
+        double d2 = 100.00;
+        float f2 = (float) d2;
+        long l2 = (long) f2;
+        int i2 = (int) l2;
+        short s2 = (short) i2;
+
+        //EM OPERAÇÕES COM TIPOS DISTINTOS, O RESULTADO É DO TIPO "MAIOR" DELES
+
+        System.out.println(s2);
+
+        //Operações entre shorts retorna inteiro
+
+        //Para garantir operações entre dois ints grandes, deve-se fazer o cast para long e armazenar o resultado em long
+    }
+
 }
