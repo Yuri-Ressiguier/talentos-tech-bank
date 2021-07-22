071b1e3c21c71bf5dac3412331437f733939817a
Yuri Ressiguier <yuri_ressiguier@id.uff.br>
2021-07-21 22:06:56 -0300

    Conexão Banco de Dados

---
+++ src/sql/ExemplosSQLite.java
@@ -0,0 +1,31 @@
+package sql;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ExemplosSQLite {
+
+
+    public static void main(String[] args) {
+        String urlPostgres = System.getenv("urlPostgres");
+        String usuario = System.getenv("usuario");
+        String senha = System.getenv("senha");
+        try (
+                Connection conn = DriverManager.getConnection(urlPostgres, usuario, senha);
+                Statement stmt = conn.createStatement()) {
+            String query = "select 1 as id, 'Anderson' as nome";
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                System.out.println("ID: " + rs.getInt("id"));
+                System.out.println("NOME: " + rs.getString("nome"));
+            }
+            rs.close();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
